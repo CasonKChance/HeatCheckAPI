74202267ddd060910c0a0454223f260512d19bb3
cchance6 <cchance6@gatech.edu>
2024-04-06 19:06:31 -0400

    Fixed urls namespace bug causing failed migrations

--- HeatCheckAPI/urls.py
+++ HeatCheckAPI/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-    path('games/', include('games.urls', namespace='games')),
-    path('courts/', include('courts.urls', namespace='courts')),
+    path('users/', include('users.urls')),
+    path('games/', include('games.urls')),
+    path('courts/', include('courts.urls')),
 ]
--- courts/urls.py
+++ courts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import CourtListCreate, CourtRetrieveUpdateDestroy, CourtListView, CourtSpecificView
 
 urlpatterns = [
-    path('courts/', CourtListCreate.as_view(), name='court-list-create'),
-    path('courts/<int:pk>/', CourtRetrieveUpdateDestroy.as_view(),
+    path('', CourtListCreate.as_view(), name='court-list-create'),
+    path('<int:pk>/', CourtRetrieveUpdateDestroy.as_view(),
          name='court-retrieve-update-destroy'),
     path('court-list/', CourtListView.as_view(), name='court-list'),
-    path('courts/<int:pk>/detail/', CourtSpecificView.as_view(),
+    path('<int:pk>/detail/', CourtSpecificView.as_view(),
          name='court-specific'),
 ]
